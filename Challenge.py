91ca7929cc8c4152bc7e737e7c45470d81a3a80e
GeneralLol <dsqchn@gmail.com>
2018-05-10 14:32:59 -0400

    Add files via upload

--- Challenge.py
+++ Challenge.py
@@ -1,96 +1,42 @@
-import sys, os, random
+import sys
 
-random.seed()
-
-def coinconverter(num, coinlist):
-    counter = 0
-    coin_counter = [0] * len(coinlist)
-    flag = False
-    #Currently not quitting this while loop correctly
-    while num != 0:
-
-        while num >= 0:
-            num -= coinlist[counter]
-            coin_counter[counter] += 1
-            flag = True
-
-        if flag:
-            num += coinlist[counter]
-
-        flag = False
-        coin_counter[counter] -= 1
-        counter += 1
-    return coin_counter
-
-def stringcomb(string):
-    length = len(string)
-    temp_string = string
-    flags = [0]*len(string)
-    #i is number of letters of the current combination
-    i = 0
-    while i < len(string):
-        pass
-
-def whilechallenge():
-    os.system('clear')
-    flag = True
-    counter = 1
-    usr_input = 'N'
-    while flag:
-        usr_input = input('Do you want to risk to double your score? [Y/N] ')
-        usr_input.upper()
-        print(flag)
-
-
-        if usr_input == 'Y' or usr_input == 'y':
-            rand = random.randrange(100)
-            if counter == 0:
-                if rand <= 75:
-                    counter = 1
-                    print ('Congrats! Double points!')
-                else:
-                    print ('Congrats! You lost!')
-                    flag = False
-            elif counter != 0:
-                if rand <= 75:
-                    counter = counter * 2
-                    print ('Congrats! Double points!')
-                else:
-                    print ('Congrats! You lost!')
-                    counter = 0
-        elif usr_input == 'N' or usr_input == 'n':
-            flag = False
+def distinctwords():
+    sig = True
+    tempwd = ''
+    wdlst  = ['']
+    while sig:
+        tempwd = input('Please input a word: ')
+        if tempwd in wdlst:
+            sig = False
+            print ('Repitition detected.')
         else:
-            print ('Wrong input. Try again.')
+            sig = True
+            wdlst.append(tempwd)
+            tempwd = ''
 
-        print('Your current score is: ', counter)
+    wdlst.pop(0)
+    term_counts = len(wdlst)
+    return term_counts
 
+def invariant(var, lst):
+    rst = False
+    tempstr = ''
+    for i in range(len(lst)-len(var)):
+        tempstr = ''
+        for j in range(i, i+len(var)):
+            tempstr = tempstr + lst[j]
+        if var == tempstr:
+            rst = True
+            break
+        else:
+            rst = False
 
+    return rst
 
-
-#Main structure to pull up other functions
-args = sys.argv
-if args[1] == '-while':
-    whilechallenge()
-    print ('Game Over')
-
-try:
-    if args[1] == '-h':
-        print ('Help')
-        print ('*********************')
-        print ('-h \t \t \t \t Get help')
-        print ('-cc \t \t \t \t Converts raw numbers to coins specified')
-        print ()
-    if args[1] == '-cc':
-        number_raw      = float(input('Please input a number: '))
-        possible_coins  = [int(x) for x in input('Please input available coins: ').split()]
-        print ('Results are: ')
-        number = int(number_raw*100)
-        coins  = coinconverter(number, possible_coins)
-        for i in range(len(possible_coins)):
-            print (possible_coins[i], '\t', coins[i])
-
-except:
-	print ('-h to get some help')
-
-sys.exit()
+flag = input('which part of the challenge do you want access to? a for distinct words, b for in function. ')
+if flag == 'a':
+    print (distinctwords())
+elif flag == 'b':
+    var = input('Please input a string. ')
+    lst = input('Please input a string. ')
+    print (invariant(var, lst))
